b4b2c93a6a922a07ca0b62a7949cdb45b9f0ccf0
XiangcenWu <93538241+XiangcenWu@users.noreply.github.com>
2024-10-10 22:08:59 +0100

    Add background and lines

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include "widget.h"
+
+#include <QApplication>
+#include <QtCore>
 
 
-int main()
+void test()
 {
-    std::cout << "hello world" << std::endl;
-    return 0;
+    QTextStream cout(stdout, QIODevice::WriteOnly);
+    QString s1 = "Hello";
+    QString s2 = "Qt";
+    QList<QString> list;
+    // stream into containers
+    list << s1 << s2;
+    // Java and STL like iterators
+    QListIterator<QString> iter(list);
+    while(iter.hasNext()) 
+    {
+        cout << iter.next();
+        if(iter.hasNext()) 
+        {
+            cout << " ";
+        }
+    }
+    cout << "!" << Qt::endl;
 }
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    test();
+    return a.exec();
+}
